8153def93dfb5723424f5c8c1f4ad871973515a1
wangjun10 <wangjun10@58ganji.com>
2018-09-18 18:25:13 +0800

    my test

---
+++ src/PostPub.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by gjfc.
+ * User: wangjun10
+ * Date: 2018/9/18
+ * Time: 9:42
+ */
+namespace Post;
+class PostPub {
+    public static function savePost(){
+            return 'save success.';
+    }
+} 
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by gjfc.
+ * User: wangjun10
+ * Date: 2018/9/18
+ * Time: 15:59
+ */
+require __DIR__.'/vendor/autoload.php';
+use Post\PostPub;
+echo PostPub::savePost();
